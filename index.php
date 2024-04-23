d7c931b2226e9653bb304370cae3c1a0ea5db79b
brandonviorato <brandonviorato@outlook.com>
2024-04-23 14:02:48 -0400

    Added some arrays

--- index.php
+++ index.php
@@ -14,7 +14,28 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 
 // Define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+    // add data to the Fat-Free hive
+    $f3->set('name', 'Tina');
+    $f3->set('color', 'Blue');
+    $f3->set('car', 'Subaru');
+
+    // define array of fruits and add to hive
+    $fruits = array('apple', 'banana', 'orange');
+    $f3->set('fruitList', $fruits);
+
+    // define array of cars
+    $cars = array('Subaru', 'Honda', 'Toyota');
+    $f3->set('carsList', $cars);
+
+    // Define associative array of students
+    $students = array('101'=>'Mason', '102'=>'Tilak', '103'=>'Sage');
+    $f3->set('studentList', $students);
+
+    // Add a number to hive
+    $f3->set('num', 5);
+
+
     // Render a view page
     $view = new Template();
     echo $view->render('views/info.html');
